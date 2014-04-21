6d8ec3ace8ed28b489deb80019ef6a09b1829d76
Pooja Mathur <puja.mathur@hotmail.com>
2014-04-22 00:29:02 +0530

    DataBase Connection

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,9 +1,9 @@
 dataSource {
     pooled = true
     jmxExport = true
-    driverClassName = "org.h2.Driver"
-    username = "sa"
-    password = ""
+    driverClassName = "com.mysql.jdbc.Driver"
+    username = "dev"
+    password = "puja"
 }
 hibernate {
     cache.use_second_level_cache = true
@@ -18,7 +18,7 @@ environments {
     development {
         dataSource {
             dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+            url = "jdbc:mysql://localhost:3306/cricosphere"
         }
     }
     test {
